d65982233bd36b67ee3d3c5f900ad459a232ba02
Guilherme Teodoro <gui.teo2@gmail.com>
2015-05-06 23:43:34 -0300

    Bootstrap

---
+++ VelhaMania/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VelhaMania
+//
+//  Created by Guilherme Teodoro on 5/6/15.
+//  Copyright (c) 2015 Little Dice Gamee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
